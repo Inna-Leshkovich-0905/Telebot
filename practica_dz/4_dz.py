7dc66d192db6fa12bc7bf17c1a8ac3204170af29
Inna Leshkovich <110292087+Inna-Leshkovich-0905@users.noreply.github.com>
2022-09-09 20:15:32 +0300

    Delete practica_dz directory

--- practica_dz/4_dz.py
+++
@@ -1,6 +0,0 @@
-a=input("Введите числа через пробел: ").split()
-c=[]
-for i in a:
-    if a.count(i)==1:
-        c.append(i)
-print("Список уникальных чисел: ", c)
--- practica_dz/6_dz_dict.py
+++
@@ -1,9 +0,0 @@
-# задача 1
-
-cars = {'BMW': ['X6', 'X7', 'Z4'], 'Tesla': ['Model S', 'Model X', 'Model Y']}
-
-for key, value in cars.items():
-    print(f'Авто: {key}, модель: {value[0]}')
-    print(f'Авто: {key}, модель: {value[-1]}')
-
-    # print(f'Авто: {key}, модель: {value[0]}', f'Авто: {key}, модель: {value[-1]} ')
